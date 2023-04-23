d6f28df579469af788a78824369b8456627c6bb0
Jens Lippert <jens-lippert01@web.de>
2023-04-23 13:27:48 +0200

    Lion now walks

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -10,6 +10,7 @@ public class Player_Movement : MonoBehaviour
     public float gravity = 20f;
     public float crouchSpeed = 10f;
     private Vector2 moveDirection = Vector2.zero;
+    private Animator animator;
 
     private CharacterController controller;
     private SpriteRenderer sprite;
@@ -17,6 +18,7 @@ public class Player_Movement : MonoBehaviour
     void Start() {
         controller = GetComponent<CharacterController>();
         sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,14 +33,23 @@ public class Player_Movement : MonoBehaviour
             if (Input.GetButton("Jump")) {
                 moveDirection.y = jumpSpeed;
             }
+        } else {
+            moveDirection.y -= gravity * Time.deltaTime;
         }
         if (moveDirection.x < 0) {
             sprite.flipX = true;
         } else if (moveDirection.x > 0) {
             sprite.flipX = false;
         }
-        moveDirection.y -= gravity * Time.deltaTime;
 
         controller.Move(moveDirection * Time.deltaTime);
+
+        // Walk-Animation
+        Debug.Log(moveDirection);
+        if(moveDirection == Vector2.zero || !controller.isGrounded) {
+            animator.SetFloat("Speed", 0);
+		} else { 
+            animator.SetFloat("Speed", Mathf.Abs(moveDirection.x));
+		}
     }
 }
